3a6cdc5b2713c096422acbb7882676e4be022271
Kate Humphrey <kate@fullstackacademy.com>
2018-03-30 15:26:31 -0400

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   module: {
     rules: [{
         test: /\.jsx?$/,
-        // exclude: /node_modules/,
+        exclude: /node_modules/,
         loader: 'babel-loader'
       }
     ]
