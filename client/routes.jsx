81c07f894fd2953ac9e755a168152b1ca1c2b573
Kate Humphrey <kate@fullstackacademy.com>
2018-03-30 14:59:37 -0400

    remove extra

--- client/routes.jsx
+++ client/routes.jsx
@@ -3,7 +3,6 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Home} from './components'
-import {me} from './store'
 
 /**
  * COMPONENT
@@ -30,17 +29,11 @@ class Routes extends React.Component {
  */
 const mapState = (state) => {
   return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
   }
 }
 
 const mapDispatch = (dispatch) => {
   return {
-    loadInitialData () {
-      dispatch(me())
-    }
   }
 }
 
